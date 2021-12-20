406a2c52fdd1d784af2e2fce92876fba9c0027aa
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2021-12-20 11:54:26 +0100

    affichage

--- calculatrice.js
+++ calculatrice.js
@@ -13,24 +13,42 @@ const nums = {
     operator: null
 }
 
+let fontsize = '3em'
+screen.style.fontsize = '3em'
 
-//Récupérer la valeur de ce qu'on a cliqué
+
+//Récupérer la valeur de ce qu'on a cliqué pour les nombres
 digits.map(digit => {
     digit.addEventListener('click',saveDigit)
 })
-
-function write(num) {
-    screen.innerText=num;
-}
 
 function saveDigit (){
     const digit = this.getAttribute('data-key');
     temp += digit;
     write(temp)
-
-
 }
 
+function write(num){
+    //console.log(formatNumber(num))
+    screen.innerText=formatNumber(num);
+}
+
+//Met des espaces entre les nombres
+function formatNumber(num) {
+    //Ca sort de l'écran
+    if (screen.clientWidth > window.innerWidth * 0.8){
+        fontsize *= 0.8
+        screen.style.fontSize = '${fontSize}em'
+    }
+
+
+    //On recoit une string or il nous faut un nombre
+    return parseFloat(num)
+        .toFixed(2)
+        .replace(/(\d)(?=(\d{3})+\.)/g, '$1 ')
+}
+
+//Récupérer la valeur de ce qu'on a cliqué pour les opérations
 operators.map(operator => {
     operator.addEventListener('click',operation)
 })
@@ -39,14 +57,9 @@ function operation() {
     console.log(this.innerText)
 }
 
+//Récupérer l'ordre d'effacer
 allClear.addEventListener('click',function () {
     console.log("clear");
-    screen.innerText = "0.00";
+    temp = 0;
+    write(temp)
 })
-
-//console.log(digits);
-
-//Afficher cette valeur
-
-
-//Réaliser le calcul 
