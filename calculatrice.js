e2d1a2f43d4f6a7950cadec06edbfbb48c922c58
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2021-12-21 09:16:44 +0100

    fin

--- calculatrice.js
+++ calculatrice.js
@@ -5,61 +5,101 @@ const operators = Array.from(document.querySelectorAll('.main_operators'))
 const allClear = document.querySelector('.clear')
 
 //Variables
-let temp = '';
+let temp = ''
 
 const nums = {
     num1: 0,
-    num2:0,
+    num2: 0,
     operator: null
 }
 
-let fontsize = '3em'
-screen.style.fontsize = '3em'
+let fontSize = 3
+screen.style.fontSize = '3em'
 
-
-//Récupérer la valeur de ce qu'on a cliqué pour les nombres
 digits.map(digit => {
-    digit.addEventListener('click',saveDigit)
+    digit.addEventListener('click', saveDigit)
 })
 
-function saveDigit (){
-    const digit = this.getAttribute('data-key');
-    temp += digit;
+operators.map(operator => {
+    operator.addEventListener('click', operation)
+})
+
+allClear.addEventListener('click', function () {
+    clear()
+    screen.innerText = '0.00'
+})
+
+function clear () {
+    temp = 0
+    nums.num1 = 0
+    nums.num2 = 0
+    nums.operator = null
+}
+
+function operation () {
+    const operator = this.getAttribute('data-key')
+
+    if (operator === '=') {
+        result(nums)
+        clear()
+    }
+
+    if (!nums.num2) {
+        nums.num2 = nums.num1
+        nums.num1 = 0
+        temp = 0
+    } else if (!nums.num1) {
+        nums.operator = operator
+    } else {
+        result(nums)
+    }
+
+    nums.operator = operator
+}
+
+function result ({ num1, num2, operator }) {
+    let sum = 0
+
+    if (operator === '+') {
+        sum = num1 + num2
+    } else if (operator === '-') {
+        sum = num2 - num1
+    } else if (operator === '*') {
+        sum = num2 * num1
+    } else if (operator === '/') {
+        sum = num2 / num1
+    }
+
+    nums.num1 = 0
+    nums.num2 = sum
+    temp = 0
+    write(sum)
+}
+
+function saveDigit () {
+    const digit = this.getAttribute('data-key')
+
+    if (digit === '.' && temp.includes('.')) return
+
+    temp += digit
+
+    nums.num1 = parseFloat(temp)
+
     write(temp)
 }
 
-function write(num){
-    //console.log(formatNumber(num))
-    screen.innerText=formatNumber(num);
+function write (num) {
+    // console.log(typeof formatNum(num))
+    screen.innerText = formatNum(num)
 }
 
-//Met des espaces entre les nombres
-function formatNumber(num) {
-    //Ca sort de l'écran
-    if (screen.clientWidth > window.innerWidth * 0.8){
-        fontsize *= 0.8
-        screen.style.fontSize = '${fontSize}em'
+function formatNum (num) {
+    if (screen.clientWidth > window.innerWidth * 0.8) {
+        fontSize *= 0.8
+        screen.style.fontSize = `${fontSize}em`
     }
 
-
-    //On recoit une string or il nous faut un nombre
     return parseFloat(num)
         .toFixed(2)
         .replace(/(\d)(?=(\d{3})+\.)/g, '$1 ')
 }
-
-//Récupérer la valeur de ce qu'on a cliqué pour les opérations
-operators.map(operator => {
-    operator.addEventListener('click',operation)
-})
-
-function operation() {
-    console.log(this.innerText)
-}
-
-//Récupérer l'ordre d'effacer
-allClear.addEventListener('click',function () {
-    console.log("clear");
-    temp = 0;
-    write(temp)
-})
